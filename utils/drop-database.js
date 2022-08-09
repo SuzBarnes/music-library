8151e1721322348da19e02f14511abdaf26becd4
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-08-09 12:20:32 +0100

    updated package.json to include pretest and fix bug

--- utils/drop-database.js
+++ utils/drop-database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const path = require('path');
 
 require('dotenv').config({
@@ -7,29 +7,11 @@ require('dotenv').config({
 
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 
-const dropDatabase = async () => {
-  try {
-    const connection = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      port: DB_PORT,
-    });
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
 
-    await connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
-  } catch (err) {
-    console.log(
-      `Your environment variables might be wrong. Please double check .env file`
-    );
-    console.log('Environment Variables are:', {
-      DB_PASSWORD,
-      DB_NAME,
-      DB_USER,
-      DB_HOST,
-      DB_PORT,
-    });
-    console.log(err);
-  }
-};
-
-dropDatabase();
+connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
